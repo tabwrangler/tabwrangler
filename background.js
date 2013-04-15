7580bdbf8134f8168ebc2b0db850ee541de2906a
Justin Sonntag <justin.sonntag@gmail.com>
2013-04-15 02:15:28 -0700

    Pinned tabs are no longer added to the tabTimes object.

--- background.js
+++ background.js
@@ -14,9 +14,7 @@ function startup() {
   TW.settings.set('lockedIds', new Array());
   
   // Move this to a function somehwere so we can restart the process.
-  chrome.tabs.query({
-    windowType: 'normal'
-  }, TW.TabManager.initTabs);
+  chrome.tabs.query({ windowType: 'normal', pinned: false }, TW.TabManager.initTabs);
   chrome.tabs.onCreated.addListener(TW.TabManager.onNewTab);
   chrome.tabs.onUpdated.addListener(TW.TabManager.updateLastAccessed);
   chrome.tabs.onRemoved.addListener(TW.TabManager.removeTab);
@@ -25,6 +23,14 @@ function startup() {
     TW.TabManager.updateLastAccessed(tabInfo['tabId'])
   });
   chrome.tabs.onReplaced.addListener(TW.TabManager.replaceTab);
+  chrome.tabs.onUpdated.addListener(function(tabId, changeInfo) {
+    if (changeInfo.pinned) {
+      TW.TabManager.removeTab(tabId);
+    } else {
+      TW.TabManager.updateLastAccessed(tabId);
+    }
+  });
+  
   window.setInterval(TW.TabManager.checkToClose, TW.settings.get('checkInterval'));
   window.setInterval(TW.TabManager.updateClosedCount, TW.settings.get('badgeCounterInterval'));
   

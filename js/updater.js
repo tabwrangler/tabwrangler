3a7b05883b0d40526ca915badd97aed846693152
Justin Sonntag <justin.sonntag@gmail.com>
2013-04-30 15:12:57 -0700

    Upped version to 3.0.

--- js/updater.js
+++ js/updater.js
@@ -36,30 +36,8 @@ TW.Updater.runUpdates = function(previous, current) {
 
 TW.Updater.updates = [
   {
-    version: '2.8.5',
-    fx: function() {},
-    finished: function() { alert("this is a test.") },
-  },
-
-  {
     version: '3.0',
     fx: function() {},
-    finished: function() {
-
-      var updateTxt = ''
-      + '<strong>Updates</strong>'
-      + '<ul>'
-      + '<li> <a target="_blank" href="http://www.jacobsingh.name/tabwrangler/release-notes">See all changes</a></li>'
-      + '<li> Right click on page to immediately close and save a tab. <span class="label label-success">Feature</span></li>'
-      + '<li> Added a pause feature <span class="label label-success">Feature</span></li>'
-      + '<li> Restoring tabs w/o using TabWrangler now removes them from Corral <span class="label label-error">Bug</span></li>'
-      + '<li> <a target="_blank" href="https://chrome.google.com/webstore/detail/egnjhciaieeiiohknchakcodbpgjnchh/reviews"> Review tab wrangler!</a></li>'
-      + '</ul>';
-
-      var notification = window.webkitNotifications.createHTMLNotification(
-        'notification.html?title=Version 2.8&message=' + updateTxt
-      );
-      notification.show();
-    }
+    finished: function() {}
   }
 ]

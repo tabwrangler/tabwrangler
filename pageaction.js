8a082144aa3e5f52d40ecf96698b7353c72328e9
Jacob Singh <jacobsingh@gmail.com>
2015-05-25 10:17:18 +0530

    Added required files for lazy loading images

---
+++ pageaction.js
@@ -0,0 +1,18 @@
+require(['menus'
+], function(menus) {
+
+  var TW = chrome.extension.getBackgroundPage().TW;
+
+  // Unpack TW.
+  var tabmanager = TW.tabmanager;
+  var settings = TW.settings;
+
+  // Append all the buttons.
+
+  pageActions = menus.getPageActions();
+
+  pageActions.each(function(action) {
+    $('pageActionButtons').append();
+  });
+
+});

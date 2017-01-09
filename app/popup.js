995c0b0a132dd281562c63f7b954fa4787602cf4
Ross Allen <rossgit@gmail.com>
2017-01-09 14:56:46 -0800

    Render corral tab with React

--- app/popup.js
+++ app/popup.js
@@ -287,12 +287,205 @@ require([
     });
   };
 
-  class CorralTab extends React.PureComponent {
+  class ClosedTabGroupHeader extends React.PureComponent {
+    constructor(props) {
+      super(props);
+      this.handleClickRestoreAll = this.handleClickRestoreAll.bind(this);
+    }
+
+    handleClickRestoreAll() {
+      this.props.onRestoreAll(this.props.title);
+    }
+
     render() {
+      return (
+        <tr className="info">
+          <td colSpan="3" className="timeGroupRow">
+            <button
+              className="btn btn-mini btn-primary pull-right"
+              onClick={this.handleClickRestoreAll}>
+              restore all
+            </button>
+            closed {this.props.title}
+          </td>
+        </tr>
+      );
+    }
+  }
+
+  class ClosedTabRow extends React.PureComponent {
+    constructor() {
+      super();
+      this.openTab = this.openTab.bind(this);
+    }
+
+    openTab(event) {
+      const {tab} = this.props;
+      event.preventDefault();
+      this.props.onOpenTab(tab.id, tab.url);
+    }
+
+    render() {
+      const {tab} = this.props;
+
+      return (
+        <tr>
+          <td>
+          </td>
+          <td>
+            <a target="_blank" href={tab.url} onClick={this.openTab}>
+              {tab.title.shorten(70)}
+            </a>
+          </td>
+          <td>
+            {$.timeago(tab.closedAt)}
+          </td>
+        </tr>
+      );
+    }
+  }
+
+  class CorralTab extends React.Component {
+    constructor() {
+      super();
+
+      this.state = {
+        closedTabGroups: [],
+        filter: '',
+      };
+
+      this.clearList = this.clearList.bind(this);
+      this.handleRestoreAllFromGroup = this.handleRestoreAllFromGroup.bind(this);
+      this.openTab = this.openTab.bind(this);
+      this.setClosedTabs = this.setClosedTabs.bind(this);
+      this.setFilter = this.setFilter.bind(this);
+    }
+
+    componentDidMount() {
+      this._corralSearch.focus();
+
+      // TODO: This is assumed to be synchronous. If it becomes async, this state needs to be
+      // hoisted so this component does not need to track whether it's mounted.
+      tabmanager.searchTabs(this.setClosedTabs);
+    }
+
+    clearList() {
+      tabmanager.closedTabs.clear();
+      tabmanager.updateClosedCount();
+      this.setState({
+        closedTabGroups: [],
+      });
+    }
+
+    handleRestoreAllFromGroup(groupTitle) {
+      const group = _.findWhere(this.state.closedTabGroups, {title: groupTitle});
+      group.tabs.forEach(tab => {
+        chrome.tabs.create({active: false, url: tab.url});
+        tabmanager.closedTabs.removeTab(tab.id);
+      });
+      tabmanager.searchTabs(this.setClosedTabs, [tabmanager.filters.keyword(this.state.filter)]);
+      this.forceUpdate();
+    }
+
+    openTab(tabId, url) {
+      chrome.tabs.create({active: false, url});
+      tabmanager.closedTabs.removeTab(tabId);
+      tabmanager.searchTabs(this.setClosedTabs, [tabmanager.filters.keyword(this.state.filter)]);
+      this.forceUpdate();
+    }
+
+    setClosedTabs(closedTabs) {
+      const now = new Date().getTime();
+      const separations = []
+      separations.push([now - (1000 * 60 * 30), 'in the last 1/2 hour']);
+      separations.push([now - (1000 * 60 * 60), 'in the last hour']);
+      separations.push([now - (1000 * 60 * 60 * 2),'in the last 2 hours']);
+      separations.push([now - (1000 * 60 * 60 * 24),'in the last day']);
+      separations.push([0, 'more than a day ago']);
+
+      function getGroup(time) {
+        let limit, text;
+        for (let i = 0; i < separations.length; i++) {
+          limit = separations[i][0];
+          text = separations[i][1];
+          if (limit < time) {
+            return text;
+          }
+        }
+      }
+
+      const closedTabGroups = [];
+      let currentGroup;
+      for (let i = 0; i < closedTabs.length; i++) {
+        var tab = closedTabs[i];
+        var timeGroup = getGroup(tab.closedAt);
+
+        if (timeGroup !== currentGroup) {
+          currentGroup = _.findWhere(closedTabGroups, {title: timeGroup});
+
+          if (currentGroup == null) {
+            currentGroup = {
+              tabs: [],
+              title: timeGroup,
+            };
+            closedTabGroups.push(currentGroup);
+          }
+        }
+
+        currentGroup.tabs.push(tab)
+      }
+
+      this.setState({closedTabGroups});
+    }
+
+    setFilter(event) {
+      const filter = event.target.value;
+      this.setState({filter});
+      tabmanager.searchTabs(this.setClosedTabs, [tabmanager.filters.keyword(filter)]);
+    }
+
+    render() {
+      const tableRows = [];
+      this.state.closedTabGroups.forEach(closedTabGroup => {
+        tableRows.push(
+          <ClosedTabGroupHeader
+            key={`ctgh-${closedTabGroup.title}`}
+            onRestoreAll={this.handleRestoreAllFromGroup}
+            title={closedTabGroup.title}
+          />
+        );
+
+        closedTabGroup.tabs.forEach(tab => {
+          tableRows.push(
+            <ClosedTabRow
+              key={`ctr-${tab.id}`}
+              onOpenTab={this.openTab}
+              tab={tab}
+            />
+          );
+        });
+      });
+
+      const messageElement = this.state.closedTabGroups.length === 0
+        ? <div id="autocloseMessage" className="alert alert-info">If tabs are closed automatically, they will be stored here</div>
+        : (
+          <div className="clearCorralMessage alert alert-info">
+            <a className="clearCorralLink" href="#" onClick={this.clearList}>Clear list</a>
+          </div>
+        );
+
       return (
         <div className="tab-pane active" id="tabCorral">
           <form className="form-search">
-            <input name="search" type="search" className="span8 corral-search search-query" placeholder="search" />
+            <input
+              className="span8 corral-search search-query"
+              name="search"
+              onChange={this.setFilter}
+              placeholder="search"
+              ref={(_corralSearch) => { this._corralSearch = _corralSearch; }}
+              type="search"
+              value={this.state.filter}
+            />
           </form>
 
           <table id="corralTable" className="table-condensed table-striped table table-bordered">
@@ -304,11 +497,11 @@ require([
               </tr>
             </thead>
             <tbody>
+              {tableRows}
             </tbody>
           </table>
 
-          <div id="autocloseMessage" className="alert alert-info">If tabs are closed automatically, they will be stored here</div>
-          <div className="clearCorralMessage alert alert-info"><a className="clearCorralLink" href="#">Clear list</a></div>
+          {messageElement}
         </div>
       );
     }
@@ -317,41 +510,11 @@ require([
   Popup.corralTab = {};
 
   Popup.corralTab.init = function(context) {
-    var self = this;
-
-    // Setup interface elements
-    $('#autocloseMessage').hide();
-    $('.clearCorralMessage').hide();
-    // @todo: use context to select table.
-    tabmanager.searchTabs(function(closedTabs) {
-      if ( closedTabs.length == 0 ) {
-        // If we have no saved closed tabs, show the help text
-        $('#autocloseMessage').show();
-      } else {
-        $('.clearCorralMessage').show();
-      }
-      return self.buildTable(closedTabs);
-    });
-    $('.clearCorralLink').click(function() {
-      tabmanager.closedTabs.clear();
-      tabmanager.updateClosedCount();
-      Popup.corralTab.init();
-      return;
-    });
-
     if(location.search !== "?foo") {
       location.search = "?foo";
       throw new Error;  // load everything on the next page;
       // stop execution on this page
     }
-
-    $('.corral-search').keyup(_.debounce(
-    function() {
-      var keyword = $(this).val();
-      tabmanager.searchTabs(self.buildTable, [tabmanager.filters.keyword(keyword)]);
-    }, 200));
-
-    $('.corral-search').delay(1000).focus();
   };
 
   Popup.corralTab.buildTable = function(closedTabs) {
@@ -369,39 +532,39 @@ require([
      */
 
     // Clear out the table.
-    var $tbody = $('#corralTable tbody');
-    $tbody.html('');
+    // var $tbody = $('#corralTable tbody');
+    // $tbody.html('');
 
-    var now = new Date().getTime();
-    var separations = []
-    separations.push([now - (1000 * 60 * 30), 'in the last 1/2 hour']);
-    separations.push([now - (1000 * 60 * 60), 'in the last hour']);
-    separations.push([now - (1000 * 60 * 60 * 2),'in the last 2 hours']);
-    separations.push([now - (1000 * 60 * 60 * 24),'in the last day']);
-    separations.push([0, 'more than a day ago']);
+    // var now = new Date().getTime();
+    // var separations = []
+    // separations.push([now - (1000 * 60 * 30), 'in the last 1/2 hour']);
+    // separations.push([now - (1000 * 60 * 60), 'in the last hour']);
+    // separations.push([now - (1000 * 60 * 60 * 2),'in the last 2 hours']);
+    // separations.push([now - (1000 * 60 * 60 * 24),'in the last day']);
+    // separations.push([0, 'more than a day ago']);
 
-    function getGroup(time) {
-      var limit, text, i;
-      for (var i = 0; i < separations.length; i++) {
-        limit = separations[i][0];
-        text = separations[i][1];
-        if (limit < time) {
-          return text;
-        }
-      }
-    }
+    // function getGroup(time) {
+    //   var limit, text, i;
+    //   for (var i = 0; i < separations.length; i++) {
+    //     limit = separations[i][0];
+    //     text = separations[i][1];
+    //     if (limit < time) {
+    //       return text;
+    //     }
+    //   }
+    // }
 
-    function createGroupRow(timeGroup, $tbody) {
-      var $tr = $('<tr class="info"></tr>');
-      var $button = $('<button class="btn btn-mini btn-primary" style="float:right;">restore all</button>').click(function() {
-        $('tr[data-group="' + timeGroup + '"]').each(function() {
-          $('a', this).click();
-        });
-      });
-      var $td = $('<td colspan=3 class="timeGroupRow"> closed ' + timeGroup + '</td>').append($button);
-      $tr.append($td);
-      $tbody.append($tr);
-    }
+    // function createGroupRow(timeGroup, $tbody) {
+    //   var $tr = $('<tr class="info"></tr>');
+    //   var $button = $('<button class="btn btn-mini btn-primary" style="float:right;">restore all</button>').click(function() {
+    //     $('tr[data-group="' + timeGroup + '"]').each(function() {
+    //       $('a', this).click();
+    //     });
+    //   });
+    //   var $td = $('<td colspan=3 class="timeGroupRow"> closed ' + timeGroup + '</td>').append($button);
+    //   $tr.append($td);
+    //   $tbody.append($tr);
+    // }
 
     function createTabRow(tab, group, $tbody) {
       // Create a new row.
@@ -440,23 +603,23 @@ require([
       //      a_title.href = urls[i];
       //    }
 
-      var $link = $('<a target="_blank" href="' + tab.url + '">' + tab.title.shorten(70) + '</a>');
+      // var $link = $('<a target="_blank" href="' + tab.url + '">' + tab.title.shorten(70) + '</a>');
 
       // Create a new tab when clicked in the background
       // Remove from the closedTabs list.
-      $link.click(function() {
-        chrome.tabs.create({active:false, url: $(this).attr('href')});
-        tabmanager.closedTabs.removeTab($(this).parent().parent().data('tabid'));
-        $(this).parent().parent().remove();
-        return false;
-      });
+      // $link.click(function() {
+      //   chrome.tabs.create({active:false, url: $(this).attr('href')});
+      //   tabmanager.closedTabs.removeTab($(this).parent().parent().data('tabid'));
+      //   $(this).parent().parent().remove();
+      //   return false;
+      // });
 
-      $tr.append($('<td></td/>').append($link));
+      // $tr.append($('<td></td>').append($link));
       // Url - not sure if we want this.
       // $tr.append($('<td>' + tab.url.shorten(70) + '</td>'));
       // time ago.
-      $tr.append('<td>' + $.timeago(tab.closedAt) + '</td>');
-      $tbody.append($tr);
+      // $tr.append('<td>' + $.timeago(tab.closedAt) + '</td>');
+      // $tbody.append($tr);
     }
 
     /**
@@ -481,12 +644,7 @@ require([
       }
 
       createTabRow(tab, currentGroup, $tbody);
-
     }
-  };
-
-  Popup.corralTab.filterByKeyword = function() {
-    var keyword = $(this).val();
   };
 
   class PauseButton extends React.PureComponent {

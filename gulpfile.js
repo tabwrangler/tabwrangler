b491019b9592a88391470e0234e0ddb9ee59815a
Ross Allen <rossgit@gmail.com>
2017-01-14 09:01:46 -0800

    Hook Webpack's watch into the `gulp watch` task
    Building is now: `gulp`
    Watching is now: `gulp watch`

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,10 @@
 const batch = require('gulp-batch');
 const gulp = require('gulp');
+const gutil = require('gulp-util');
 const watch = require('gulp-watch');
+const webpack = require('webpack');
+
+const webpackConfig = require('./webpack.config.js');
 
 // Copy all files except for *.js ones
 gulp.task('cp', function() {
@@ -18,17 +22,41 @@ gulp.task('cp-lib', function() {
     .pipe(gulp.dest('dist/lib'));
 });
 
-// Batch every 500ms since switching between code and browser takes at least that long, save a few
+gulp.task('webpack', function(done) {
+  webpack(webpackConfig).run(function(err, stats) {
+    if (err) throw new gutil.PluginError("webpack", err);
+    gutil.log("[webpack]", stats.toString({colors: true}));
+    done();
+  });
+});
+
+gulp.task('webpack:watch', function(done) {
+  let firstRun = true;
+  webpack(Object.assign({}, {watch: true}, webpackConfig), function(err, stats) {
+    if (err) throw new gutil.PluginError('webpack', err);
+    gutil.log('[webpack]', stats.toString({
+      chunks: false, // Limit chunk information output; it's slow and not too useful
+      colors: true
+    }));
+
+    // Call Gulp's `done` callback only once per watch. Calling it more than once is an error.
+    if (firstRun) {
+      firstRun = false;
+      done();
+    }
+  });
+});
+
+// Batch every 250ms since switching between code and browser takes at least that long, save a few
 // saves.
-const WATCH_OPTIONS = {timeout: 500};
+const WATCH_OPTIONS = {timeout: 250};
 
 // Watch and re-compile when in development.
 gulp.task('watch', function() {
   watch('app/**/!(*.js)', batch(WATCH_OPTIONS, function(events, done) {
     gulp.start('cp', done);
   }));
-
-  gulp.start('default');
+  gulp.start(['cp', 'cp-lib', 'webpack:watch']);
 });
 
-gulp.task('default', ['cp', 'cp-lib']);
+gulp.task('default', ['cp', 'cp-lib', 'webpack']);

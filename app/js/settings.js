b6d96494e4b0dc4561758f4091829485580a5fa3
Ross Allen <rossgit@gmail.com>
2017-01-16 14:09:05 -0800

    Enable Flow in settings.js
    Object literals are frozen by default, so move all functions to the
    original declaration.

--- app/js/settings.js
+++ app/js/settings.js
@@ -1,6 +1,7 @@
 'use strict';
 
 /* global chrome */
+/* @flow */
 
 import tabmanager from './tabmanager';
 
@@ -8,6 +9,8 @@ import tabmanager from './tabmanager';
  * @type {Object}
  */
 const Settings = {
+  cache: {},
+
   defaults: {
     checkInterval: 5000, // How often we check for old tabs.
     badgeCounterInterval: 6000, // How often we update the # of closed tabs in the badge.
@@ -20,6 +23,7 @@ const Settings = {
     whitelist: ["chrome://*"], // An array of patterns to check against.  If a URL matches a pattern, it is never locked.
     paused: false, // If TabWrangler is paused (won't count down)
   },
+
   // Gets all settings from sync and stores them locally.
   init: function() {
     const self = this;
@@ -38,133 +42,119 @@ const Settings = {
       }
     });
   },
-  cache: {},
-};
 
-/**
- * Returns the number of milliseconds that tabs should stay open for without being used.
- *
- * @return {Number}
- */
-Settings.stayOpen = function() {
-  return parseInt(this.get('minutesInactive'), 10) * 60000;
-};
+  /**
+   * Either calls a getter function or retunrs directly from storage.
+   * @param key
+   * @param fx
+   *  Callback function after value is received.
+   * @return {*}
+   */
+  get(key: string): mixed {
+    if (typeof this[key] == 'function') {
+      return this[key]();
+    }
+    return this.cache[key];
+  },
 
-/**
- *
- * @param value
- * @see Settings.set
- */
-Settings.setminutesInactive = function(value) {
-  if ( isNaN(parseInt(value, 10)) || parseInt(value, 10) <= 0 || parseInt(value, 10) > 720 ){
-    throw Error("Minutes Inactive must be greater than 0 and less than 720");
-  }
-  // Reset the tabTimes since we changed the setting
-  tabmanager.tabTimes = {};
-  chrome.tabs.query({windowType: 'normal'}, tabmanager.initTabs);
+  /**
+   * Sets a value in localStorage.  Can also call a setter.
+   *
+   * If the value is a struct (object or array) it is JSONified.
+   *
+   * @param key
+   *  Settings keyword string.
+   * @param value
+   * @return {*}
+   */
+  set(key: string, value: mixed) {
+    // Magic setter functions are set{fieldname}
+    if (typeof this["set" + key] == 'function') {
+      return this["set" + key](value);
+    }
+    Settings.setValue(key, value);
+  },
 
-  Settings.setValue('minutesInactive', value);
-};
+  /**
+   *
+   * @param value
+   * @see Settings.set
+   */
+  setmaxTabs(value: string) {
+    if ( isNaN(parseInt(value, 10)) || parseInt(value, 10) <= 1 || parseInt(value, 10) > 500 ){
+      throw Error("Max tabs must be a number between 1 and 500. Setting this too high can cause performance issues");
+    }
+    Settings.setValue('maxTabs', value);
+  },
 
-/**
- *
- * @param value
- * @see Settings.set
- */
-Settings.setminTabs = function(value) {
-  if ( isNaN(parseInt(value, 10)) || parseInt(value, 10) <= 0 || parseInt(value, 10) > 30 ){
-    throw Error("Minimum tabs must be a number between 0 and 30");
-  }
-  Settings.setValue('minTabs', value);
-};
+  /**
+   *
+   * @param value
+   * @see Settings.set
+   */
+  setminTabs(value: string) {
+    if ( isNaN(parseInt(value, 10)) || parseInt(value, 10) <= 0 || parseInt(value, 10) > 30 ){
+      throw Error("Minimum tabs must be a number between 0 and 30");
+    }
+    Settings.setValue('minTabs', value);
+  },
 
-/**
- *
- * @param value
- * @see Settings.set
- */
-Settings.setmaxTabs = function(value) {
-  if ( isNaN(parseInt(value, 10)) || parseInt(value, 10) <= 1 || parseInt(value, 10) > 500 ){
-    throw Error("Max tabs must be a number between 1 and 500. Setting this too high can cause performance issues");
-  }
-  Settings.setValue('maxTabs', value);
-};
+  /**
+   *
+   * @param value
+   * @see Settings.set
+   */
+  setminutesInactive(value: string) {
+    if ( isNaN(parseInt(value, 10)) || parseInt(value, 10) <= 0 || parseInt(value, 10) > 720 ){
+      throw Error("Minutes Inactive must be greater than 0 and less than 720");
+    }
+    // Reset the tabTimes since we changed the setting
+    tabmanager.tabTimes = {};
+    chrome.tabs.query({windowType: 'normal'}, tabmanager.initTabs);
 
-/**
- *
- * @param value
- * @see Settings.set
- */
-Settings.setshowBadgeCount = function(value) {
-  if (value === false) {
-    // Clear out the current badge setting
-    chrome.browserAction.setBadgeText({text: ""});
-  }
-  Settings.setValue('showBadgeCount', value);
-};
+    Settings.setValue('minutesInactive', value);
+  },
 
-/**
- * @param value
- * @see Settings.set
- */
-Settings.setwhitelist = function(value) {
-  // It should be an array, but JS is stupid: http://javascript.crockford.com/remedial.html
-  if (typeof(value) != 'object') {
-    throw new Error('Whitelist should be an array, ' + typeof(value) + ' given');
-  }
+  setpaused(value: boolean) {
+    console.log(value);
+    if (value === false) {
+      // The user has just unpaused, immediately set all tabs to the current time
+      // so they will not be closed.
+      chrome.tabs.query({
+        windowType: 'normal',
+      }, tabmanager.initTabs);
+    }
+    Settings.setValue('paused', value);
+  },
 
-  Settings.setValue('whitelist', value);
-};
+  /**
+   *
+   * @param value
+   * @see Settings.set
+   */
+  setshowBadgeCount(value: boolean) {
+    if (value === false) {
+      // Clear out the current badge setting
+      chrome.browserAction.setBadgeText({text: ""});
+    }
+    Settings.setValue('showBadgeCount', value);
+  },
 
-Settings.setpaused = function(value) {
-  console.log(value);
-  if (value === false) {
-    // The user has just unpaused, immediately set all tabs to the current time
-    // so they will not be closed.
-    chrome.tabs.query({
-    windowType: 'normal',
-  }, tabmanager.initTabs);
-  }
-  Settings.setValue('paused', value);
-};
+  setValue(key: string, value: mixed, fx?: () => void) {
+    const items = {};
+    this.cache[key] = value;
+    items[key] = value;
+    chrome.storage.sync.set(items, fx);
+  },
 
-/**
- * Either calls a getter function or retunrs directly from storage.
- * @param key
- * @param fx
- *  Callback function after value is received.
- * @return {*}
- */
-Settings.get = function(key) {
-  if (typeof this[key] == 'function') {
-    return this[key]();
-  }
-  return this.cache[key];
-};
-
-Settings.setValue = function (key, value, fx) {
-  const items = {};
-  this.cache[key] = value;
-  items[key] = value;
-  chrome.storage.sync.set(items, fx);
-};
-
-/**
- * Sets a value in localStorage.  Can also call a setter.
- *
- * If the value is a struct (object or array) it is JSONified.
- *
- * @param key
- *  Settings keyword string.
- * @param value
- * @return {*}
- */
-Settings.set = function(key, value) {
-  // Magic setter functions are set{fieldname}
-  if (typeof this["set" + key] == 'function') {
-    return this["set" + key](value);
-  }
-  Settings.setValue(key, value);
+  /**
+   * Returns the number of milliseconds that tabs should stay open for without being used.
+   *
+   * @return {Number}
+   */
+  stayOpen() {
+    return parseInt(this.get('minutesInactive'), 10) * 60000;
+  },
 };
 
 export default Settings;

ad055127c2395fcbe6aa22e35e2d0f09c75f030a
Ross Allen <rossgit@gmail.com>
2017-07-19 00:32:39 -0700

    Flowify `truncateString`, get some coverage

--- app/js/truncateString.js
+++ app/js/truncateString.js
@@ -1,3 +1,5 @@
-export default function truncateString(str, length) {
+/* @flow */
+
+export default function truncateString(str: ?string, length: number): ?string {
   return str == null || str.length <= (length + 3) ? str : `${str.substring(0, length)}...`;
 }

d7f586342353f97c566f90c9c44f07466ba38580
Ross Allen <rossgit@gmail.com>
2017-09-18 00:50:35 +0800

    Flowify updater.js
    * Replace deprecated `chrome.app` call with `chrome.runtime`
    * Use `currentVersion` in a string only when its guaranteed to be
      defined
    * Use `Object.keys` rather than iterating an Object to let the key be
      a number or a string

--- app/js/updater.js
+++ app/js/updater.js
@@ -1,3 +1,4 @@
+/* @flow */
 /* global TW */
 
 /**
@@ -14,7 +15,7 @@ const Updater = {
       let currentVersion;
 
       // The version from the manifest file
-      const manifestVersion = parseFloat(chrome.app.getDetails().version);
+      const manifestVersion = parseFloat(chrome.runtime.getManifest().version);
 
       // If items[version] is undefined, the app has either not been installed,
       // or it is an upgrade from when we were not storing the version.
@@ -22,10 +23,11 @@ const Updater = {
         currentVersion = items['version'];
       }
 
-      if (!currentVersion) {
-        // Hardcoded here to make the code simpler.
-        // This is the first update for users upgrading from when we didn't store
-        // a version.
+      if (currentVersion) {
+        console.log('Updating from ' + currentVersion + ' to ' + manifestVersion);
+      } else {
+        // Hardcoded here to make the code simpler. This is the first update for users upgrading
+        // from when we didn't store a version.
         if (localStorage['minutes_inactive']) {
           // This is the ancient 1.x version
           this.updates[2.1].fx();
@@ -34,37 +36,35 @@ const Updater = {
           // This is an update from the 2.1 version
           currentVersion = 2.1;
         }
+        console.log('Updating to ' + manifestVersion);
       }
-      console.log('Updating from ' + currentVersion + ' to ' + manifestVersion);
       self.runUpdates(currentVersion, manifestVersion);
     });
   },
-  runUpdates(currentVersion, manifestVersion) {
+  runUpdates(currentVersion: number, manifestVersion: number) {
     const self = this;
     if (!currentVersion) {
       chrome.storage.sync.set({
         'version': manifestVersion,
       });
     } else if (currentVersion < manifestVersion) {
-      for (const i in this.updates) {
-        if (this.updates.hasOwnProperty(i)) {
-          if (i > currentVersion) {
-            this.updates[i].fx();
-          }
-
-          // This is the version we are updating to.
-          if (i === manifestVersion) {
-            // Post 2.0 updates.
-            chrome.storage.sync.set({
-              'version': manifestVersion,
-            },function() {
-              if (typeof self.updates[i].finished == 'function') {
-                self.updates[i].finished();
-              }
-            });
-          }
+      Object.keys(this.updates).forEach(i => {
+        if (i > currentVersion) {
+          this.updates[i].fx();
         }
-      }
+
+        // This is the version we are updating to.
+        if (i === manifestVersion) {
+          // Post 2.0 updates.
+          chrome.storage.sync.set({
+            'version': manifestVersion,
+          }, function() {
+            if (typeof self.updates[i].finished == 'function') {
+              self.updates[i].finished();
+            }
+          });
+        }
+      });
     }
   },
 };

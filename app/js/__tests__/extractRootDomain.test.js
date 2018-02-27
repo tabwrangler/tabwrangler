f38809066cd6a5d1351c6f8d42f8ea5d5f832d29
Ross Allen <rossgit@gmail.com>
2018-02-27 23:26:55 +0800

    Sort subdomains correctly for 2-char TLDs like '.io'
    Add a test to ensure this thing stays fixed.

---
+++ app/js/__tests__/extractRootDomain.test.js
@@ -0,0 +1,15 @@
+import extractRootDomain from '../extractRootDomain';
+
+describe('extractRootDomain', () => {
+  test('extracts from 2-character TLDs', () => {
+    expect(extractRootDomain('https://ssorallen.github.io/react-todos/')).toEqual('github.io');
+  });
+
+  test('extracts country code TLDs', () => {
+    expect(extractRootDomain('https://www.bbc.co.uk/')).toEqual('bbc.co.uk');
+  });
+
+  test('extracts one of those shenanigans TLDs', () => {
+    expect(extractRootDomain('http://www.diy.guru/')).toEqual('diy.guru');
+  });
+});
--- app/js/extractRootDomain.js
+++ app/js/extractRootDomain.js
@@ -2,6 +2,7 @@
 
 import extractHostname from './extractHostname';
 
+// Original code from https://stackoverflow.com/a/23945027/368697.
 export default function extractRootDomain(url: string): string {
   let domain = extractHostname(url);
   const splitArr = domain.split('.');
@@ -11,7 +12,7 @@ export default function extractRootDomain(url: string): string {
   if (arrLen > 2) {
     domain = splitArr[arrLen - 2] + '.' + splitArr[arrLen - 1];
     //check to see if it's using a Country Code Top Level Domain (ccTLD) (i.e. ".me.uk")
-    if (splitArr[arrLen - 1].length === 2 && splitArr[arrLen - 1].length === 2) {
+    if (splitArr[arrLen - 1].length === 2 && splitArr[arrLen - 2].length === 2) {
       //this is using a ccTLD
       domain = splitArr[arrLen - 3] + '.' + domain;
     }

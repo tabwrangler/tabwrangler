7fff5b1be3f3ddbac9fa84e60f45cda3f83f00f1
Ross Allen <rossgit@gmail.com>
2018-07-10 18:54:57 +0800

    Use extracted storage NPM package to version easily

--- app/js/configureStore.js
+++ app/js/configureStore.js
@@ -1,64 +1,26 @@
 /* @flow */
 
+import { applyMiddleware, combineReducers } from 'redux';
+import { localStorage, syncStorage } from 'redux-persist-webextension-storage';
 import { persistReducer, persistStore } from 'redux-persist';
-import { combineReducers } from 'redux';
 import { createStore } from 'redux';
 import localStorageReducer from './reducers/localStorageReducer';
+import logger from 'redux-logger';
 import syncStorageReducer from './reducers/syncStorageReducer';
 import tempStorageReducer from './reducers/tempStorageReducer';
-
-function createChromeStorage(type: 'local' | 'sync') {
-  return {
-    getItem(key: string) {
-      return new Promise((resolve, reject) => {
-        chrome.storage[type].get(key, value => {
-          if (chrome.runtime.lastError == null) {
-            // Chrome Storage returns the value in an Object of with its original key. Unwrap the
-            // value from the returned Object to match the `getItem` API.
-            resolve(value[key]);
-          } else {
-            reject();
-          }
-        });
-      });
-    },
-    removeItem(key: string) {
-      return new Promise((resolve, reject) => {
-        chrome.storage[type].remove(key, () => {
-          if (chrome.runtime.lastError == null) {
-            resolve();
-          } else {
-            reject();
-          }
-        });
-      });
-    },
-    setItem(key: string, value: mixed) {
-      return new Promise((resolve, reject) => {
-        chrome.storage[type].set({ [key]: value }, () => {
-          if (chrome.runtime.lastError == null) {
-            resolve();
-          } else {
-            reject();
-          }
-        });
-      });
-    },
-  };
-}
 
 const localStoragePersistConfig = {
   debug: true,
   key: 'localStorage',
   serialize: false,
-  storage: createChromeStorage('local'),
+  storage: localStorage,
 };
 
 const syncStoragePersistConfig = {
   debug: true,
   key: 'syncStorage',
   serialize: false,
-  storage: createChromeStorage('sync'),
+  storage: syncStorage,
 };
 
 const rootReducer = combineReducers({
@@ -68,7 +30,7 @@ const rootReducer = combineReducers({
 });
 
 export default function() {
-  const store = createStore(rootReducer);
+  const store = createStore(rootReducer, applyMiddleware(logger));
   return {
     persistor: persistStore(store),
     store,

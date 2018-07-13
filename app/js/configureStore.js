5d292b002d8579f798c16bf7beed0a768f33e5f9
Ross Allen <rossgit@gmail.com>
2018-07-13 18:10:19 +0800

    Fill in store with localStorage if no state
    This is the case of migrating from pre-Redux to the new Redux world.

--- app/js/configureStore.js
+++ app/js/configureStore.js
@@ -1,22 +1,19 @@
 /* @flow */
 
-import { applyMiddleware, combineReducers } from 'redux';
+import { applyMiddleware, combineReducers, createStore } from 'redux';
 import { localStorage, syncStorage } from 'redux-persist-webextension-storage';
 import { persistReducer, persistStore } from 'redux-persist';
-import { createStore } from 'redux';
 import localStorageReducer from './reducers/localStorageReducer';
 import logger from 'redux-logger';
 import syncStorageReducer from './reducers/syncStorageReducer';
 import tempStorageReducer from './reducers/tempStorageReducer';
 
 const localStoragePersistConfig = {
-  debug: true,
   key: 'localStorage',
-  migrate(state, currentVersion) {
-    // The first time this code is run there will be no redux-persist version of the state, and the
-    // `currentVersion` will be a bogus -1. In that case, return the full contents of storage to be
-    // the initial state.
-    if (currentVersion === -1) {
+  migrate(state) {
+    // The first time this code is run there will be no redux-persist version of the state. In that
+    // case, return the full contents of storage to be the initial state.
+    if (state == null) {
       return new Promise(resolve => {
         // $FlowFixMe `chrome.storage.local.get` accepts `null`, but the types are incorrect.
         chrome.storage.local.get(null, items => {

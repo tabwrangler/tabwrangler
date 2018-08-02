a2e42e683985fb9a271f2e6ce4f5aaf8b6a923ed
Ross Allen <rossgit@gmail.com>
2018-08-02 22:07:38 +0800

    Prettify gulpfile.js (no functional changes)

--- gulpfile.js
+++ gulpfile.js
@@ -20,70 +20,74 @@ gulp.task('clean', function(done) {
 });
 
 gulp.task('lint', function() {
-  return gulp.src(['**/*.js', `!${DIST_DIRECTORY}/**`, '!node_modules/**', '!coverage/**'])
-    // eslint() attaches the lint output to the "eslint" property
-    // of the file object so it can be used by other modules.
-    .pipe(eslint())
-    // eslint.format() outputs the lint results to the console.
-    // Alternatively use eslint.formatEach() (see Docs).
-    .pipe(eslint.format())
-    // To have the process exit with an error code (1) on
-    // lint error, return the stream and pipe to failAfterError last.
-    .pipe(eslint.failAfterError());
+  return (
+    gulp
+      .src(['**/*.js', `!${DIST_DIRECTORY}/**`, '!node_modules/**', '!coverage/**'])
+      // eslint() attaches the lint output to the "eslint" property
+      // of the file object so it can be used by other modules.
+      .pipe(eslint())
+      // eslint.format() outputs the lint results to the console.
+      // Alternatively use eslint.formatEach() (see Docs).
+      .pipe(eslint.format())
+      // To have the process exit with an error code (1) on
+      // lint error, return the stream and pipe to failAfterError last.
+      .pipe(eslint.failAfterError())
+  );
 });
 
-gulp.task('test', function () {
+gulp.task('test', function() {
   process.env.NODE_ENV = 'test';
-
-  return gulp.src('app/js/__tests__').pipe(jest(Object.assign({}, {
-    config: {
-      transformIgnorePatterns: [
-        '<rootDir>/dist/', '<rootDir>/node_modules/',
-      ],
-      transform: {
-        '^.+\\.jsx?$': 'babel-jest',
-      },
-      verbose: true,
-      automock: false,
-    },
-  })));
+  return gulp.src('app/js/__tests__').pipe(
+    jest(
+      Object.assign(
+        {},
+        {
+          config: {
+            transformIgnorePatterns: ['<rootDir>/dist/', '<rootDir>/node_modules/'],
+            transform: {
+              '^.+\\.jsx?$': 'babel-jest',
+            },
+            verbose: true,
+            automock: false,
+          },
+        }
+      )
+    )
+  );
 });
 
 function webpackLog(stats) {
-  gutil.log('[webpack]', stats.toString({
-    chunks: false, // Limit chunk information output; it's slow and not too useful
-    colors: true,
-    modules: false,
-  }));
+  gutil.log(
+    '[webpack]',
+    stats.toString({
+      chunks: false, // Limit chunk information output; it's slow and not too useful
+      colors: true,
+      modules: false,
+    })
+  );
 }
 
 gulp.task('webpack', function(done) {
-  return webpack(
-    webpackConfig,
-    function(err, stats) {
-      if (err) throw new gutil.PluginError('webpack', err);
-      webpackLog(stats);
-      done();
-    }
-  );
+  return webpack(webpackConfig, function(err, stats) {
+    if (err) throw new gutil.PluginError('webpack', err);
+    webpackLog(stats);
+    done();
+  });
 });
 
 gulp.task('webpack:production', function(done) {
-  return webpack(
-    webpackProductionConfig,
-    function(err, stats) {
-      if (err) throw new gutil.PluginError('webpack', err);
-      webpackLog(stats);
-      done();
-    }
-  );
+  return webpack(webpackProductionConfig, function(err, stats) {
+    if (err) throw new gutil.PluginError('webpack', err);
+    webpackLog(stats);
+    done();
+  });
 });
 
 gulp.task('webpack:watch', function(done) {
   let firstRun = true;
   return webpack(
-    webpackConfig.map(function (platformConfig) {
-      return Object.assign({}, {watch: true}, platformConfig);
+    webpackConfig.map(function(platformConfig) {
+      return Object.assign({}, { watch: true }, platformConfig);
     }),
     function(err, stats) {
       if (err) throw new gutil.PluginError('webpack', err);
@@ -113,15 +117,9 @@ gulp.task('watch', function(done) {
 });
 
 gulp.task('release', function(done) {
-  runSequence(
-    'clean',
-    'lint',
-    'test',
-    'webpack:production',
-    function() {
-      done();
-    }
-  );
+  runSequence('clean', 'lint', 'test', 'webpack:production', function() {
+    done();
+  });
 });
 
 gulp.task('default', function(done) {

e5e34e9b54f0ceb0e0833a62d34f82de3c98b0a1
Ross Allen <rossgit@gmail.com>
2018-08-07 10:01:30 +0800

    Migrate only if pre-v6 keys exist, prevent overwrite
    Only migrate data if pre-v6 keys exist. If the state is passed in as
    void but there are no keys to migrate, then do nothing rather than
    blindly overwrite that incoming state.
    
    Addresses #241

--- app/js/configureStore.js
+++ app/js/configureStore.js
@@ -7,6 +7,14 @@ import localStorageReducer from './reducers/localStorageReducer';
 import tempStorageReducer from './reducers/tempStorageReducer';
 import thunk from 'redux-thunk';
 
+const PRE_V6_STORAGE_KEYS = [
+  'installDate',
+  'savedTabs',
+  'totalTabsRemoved',
+  'totalTabsUnwrangled',
+  'totalTabsWrangled',
+];
+
 const localStoragePersistConfig = {
   key: 'localStorage',
   migrate(state) {
@@ -14,10 +22,21 @@ const localStoragePersistConfig = {
     // case, return the full contents of storage to be the initial state.
     if (state == null) {
       return new Promise(resolve => {
-        chrome.storage.local.get(null, items => {
-          // Remove old data from the store once it's been migrated.
-          chrome.storage.local.remove(Object.keys(items));
-          resolve(items);
+        chrome.storage.local.get(PRE_V6_STORAGE_KEYS, items => {
+          if (PRE_V6_STORAGE_KEYS.some(key => key !== 'installDate' && items[key] == null)) {
+            // If there's nothing left in the store, then there's something unexpected going on with
+            // react-redux and/or the Chrome/Firefox store. If any of the keys are null, then do
+            // nothing because we don't want to wipe out the current state if a timeout or some
+            // other exception occurred that is causing this error.
+            //
+            // * 'installDate' is an exception because it was never properly written to storage. If
+            //   it is void, let the migration still happen.
+            resolve(state);
+          } else {
+            // Remove old data from the store once it's been migrated.
+            chrome.storage.local.remove(Object.keys(items));
+            resolve(items);
+          }
         });
       });
     } else {

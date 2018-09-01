tabwrangler/tabwrangler

commit 5f9d302d35473aed982c4ec543abd3b0a6a90949

Update badge count to always satisfy user setting
Update the badge count after reading from the async storage to ensure
the badge count stays hidden when the user wants it to.

Fixes #232

app/js/settings.js
@@ -21,8 +21,8 @@ const Settings = {
     // An array of tabids which have been explicitly locked by the user.
     lockedIds: [],
 
-    maxTabs: 100,
     // Max number of tabs stored before the list starts getting truncated.
+    maxTabs: 100,
 
     // Stop acting if there are only minTabs tabs open.
     minTabs: 5,
@@ -62,6 +62,11 @@ const Settings = {
       for (const i in items) {
         if (items.hasOwnProperty(i)) {
           this.cache[i] = items[i];
+
+          // Because the badge count is external state, this side effect must be run once the value
+          // is read from storage. This could more elequently be handled in a reducer, but place it
+          // here to make minimal changes while correctly updating the badge count.
+          if (i === 'showBadgeCount') tabmanager.updateClosedCount();
         }
       }
     });
@@ -182,12 +187,8 @@ const Settings = {
   },
 
   setshowBadgeCount(value: boolean) {
-    if (value === false) {
-      // Clear out the current badge setting
-      chrome.browserAction.setBadgeText({ text: '' });
-    }
-    Settings.setValue('showBadgeCount', value);
     tabmanager.updateClosedCount();
+    Settings.setValue('showBadgeCount', value);
   },
 
   setValue(key: string, value: mixed, fx?: () => void) {

app/js/tabmanager.js
@@ -221,11 +221,14 @@ const TabManager = {
   },
 
   updateClosedCount() {
-    if (TW.settings.get('showBadgeCount') === false) return;
-    const savedTabsLength = TW.store.getState().localStorage.savedTabs.length;
-    chrome.browserAction.setBadgeText({
-      text: savedTabsLength.length === 0 ? '' : savedTabsLength.toString(),
-    });
+    let text;
+    if (TW.settings.get('showBadgeCount')) {
+      const savedTabsLength = TW.store.getState().localStorage.savedTabs.length;
+      text = savedTabsLength.length === 0 ? '' : savedTabsLength.toString();
+    } else {
+      text = '';
+    }
+    chrome.browserAction.setBadgeText({ text });
   },
 
   // `addListener` intersection results in incorrect function type

commit 0eca989be8aceaed3d78923675a3420426a54c4f

Hide closed count badge according to setting
This was incorrectly broken in commit
5f9d302d35473aed982c4ec543abd3b0a6a90949. The `updateClosedCount`
statement should not have been moved.

app/js/settings.js
@@ -187,8 +187,8 @@ const Settings = {
   },
 
   setshowBadgeCount(value: boolean) {
-    tabmanager.updateClosedCount();
     Settings.setValue('showBadgeCount', value);
+    tabmanager.updateClosedCount();
   },
 
   setValue(key: string, value: mixed, fx?: () => void) {
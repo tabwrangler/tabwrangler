64e0d161ee58a5d6486a60745201efe5003cd22a
Ross Allen <reallen@gmail.com>
2019-07-09 08:50:36 -0400

    Minify CSS with optimize-css-assets-webpack-plugin

--- webpack.production.config.js
+++ webpack.production.config.js
@@ -1,6 +1,7 @@
 /* eslint-env node */
 
 const ArchivePlugin = require('webpack-archive-plugin');
+const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 const developmentConfig = require('./webpack.config.js');
 const packageJson = require('./package.json');
 
@@ -9,6 +10,7 @@ module.exports = developmentConfig.map(function(platformConfig) {
     devtool: false,
     mode: 'production',
     plugins: platformConfig.plugins.concat([
+      new OptimizeCssAssetsPlugin(),
       new ArchivePlugin({
         format: 'zip',
         output: `tabwrangler-${platformConfig.name}-${packageJson.version}`,

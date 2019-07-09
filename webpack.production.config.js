646d364cbfc17b991b8f6585e7d7e13e64cc29e5
Ross Allen <reallen@gmail.com>
2019-07-09 08:45:43 -0400

    Upgrade Webpack for better JS compression

--- webpack.production.config.js
+++ webpack.production.config.js
@@ -3,19 +3,12 @@
 const ArchivePlugin = require('webpack-archive-plugin');
 const developmentConfig = require('./webpack.config.js');
 const packageJson = require('./package.json');
-const webpack = require('webpack');
 
 module.exports = developmentConfig.map(function(platformConfig) {
   return Object.assign({}, platformConfig, {
     devtool: false,
     mode: 'production',
     plugins: platformConfig.plugins.concat([
-      // Set `NODE_ENV` to production to compile out React development-only code.
-      new webpack.DefinePlugin({
-        'process.env': {
-          NODE_ENV: JSON.stringify('production'),
-        },
-      }),
       new ArchivePlugin({
         format: 'zip',
         output: `tabwrangler-${platformConfig.name}-${packageJson.version}`,

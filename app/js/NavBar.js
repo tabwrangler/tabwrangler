e691de685fde4c5660334887ee3c76432bcb29b6
Ross Allen <reallen@gmail.com>
2019-07-09 23:08:20 -0400

    Use functional comp. for NavBar, less code

--- app/js/NavBar.js
+++ app/js/NavBar.js
@@ -12,68 +12,66 @@ type Props = {
   onClickTab: (tabId: NavBarTabID) => void,
 };
 
-export default class NavBar extends React.PureComponent<Props> {
-  handleClickAboutTab = (event: SyntheticMouseEvent<HTMLElement>) => {
+export default function NavBar(props: Props) {
+  function handleClickAboutTab(event: SyntheticMouseEvent<HTMLElement>) {
     event.preventDefault();
-    this.props.onClickTab('about');
-  };
-
-  handleClickCorralTab = (event: SyntheticMouseEvent<HTMLElement>) => {
-    event.preventDefault();
-    this.props.onClickTab('corral');
-  };
-
-  handleClickLockTab = (event: SyntheticMouseEvent<HTMLElement>) => {
-    event.preventDefault();
-    this.props.onClickTab('lock');
-  };
-
-  handleClickOptionsTab = (event: SyntheticMouseEvent<HTMLElement>) => {
-    event.preventDefault();
-    this.props.onClickTab('options');
-  };
-
-  render() {
-    return (
-      <>
-        <div className="nav-bar--buttons">
-          <PauseButton />
-        </div>
-        <ul className="nav nav-tabs">
-          <li className="nav-item">
-            <a
-              className={cx('nav-link', { active: this.props.activeTabId === 'corral' })}
-              href="#corral"
-              onClick={this.handleClickCorralTab}>
-              {chrome.i18n.getMessage('tabCorral_name')}
-            </a>
-          </li>
-          <li className="nav-item">
-            <a
-              className={cx('nav-link', { active: this.props.activeTabId === 'lock' })}
-              href="#lock"
-              onClick={this.handleClickLockTab}>
-              {chrome.i18n.getMessage('tabLock_name')}
-            </a>
-          </li>
-          <li className="nav-item">
-            <a
-              className={cx('nav-link', { active: this.props.activeTabId === 'options' })}
-              href="#options"
-              onClick={this.handleClickOptionsTab}>
-              {chrome.i18n.getMessage('options_name')}
-            </a>
-          </li>
-          <li className="nav-item">
-            <a
-              className={cx('nav-link', { active: this.props.activeTabId === 'about' })}
-              href="#about"
-              onClick={this.handleClickAboutTab}>
-              {chrome.i18n.getMessage('about_name')}
-            </a>
-          </li>
-        </ul>
-      </>
-    );
+    props.onClickTab('about');
   }
+
+  function handleClickCorralTab(event: SyntheticMouseEvent<HTMLElement>) {
+    event.preventDefault();
+    props.onClickTab('corral');
+  }
+
+  function handleClickLockTab(event: SyntheticMouseEvent<HTMLElement>) {
+    event.preventDefault();
+    props.onClickTab('lock');
+  }
+
+  function handleClickOptionsTab(event: SyntheticMouseEvent<HTMLElement>) {
+    event.preventDefault();
+    props.onClickTab('options');
+  }
+
+  return (
+    <>
+      <div className="nav-bar--buttons">
+        <PauseButton />
+      </div>
+      <ul className="nav nav-tabs">
+        <li className="nav-item">
+          <a
+            className={cx('nav-link', { active: props.activeTabId === 'corral' })}
+            href="#corral"
+            onClick={handleClickCorralTab}>
+            {chrome.i18n.getMessage('tabCorral_name')}
+          </a>
+        </li>
+        <li className="nav-item">
+          <a
+            className={cx('nav-link', { active: props.activeTabId === 'lock' })}
+            href="#lock"
+            onClick={handleClickLockTab}>
+            {chrome.i18n.getMessage('tabLock_name')}
+          </a>
+        </li>
+        <li className="nav-item">
+          <a
+            className={cx('nav-link', { active: props.activeTabId === 'options' })}
+            href="#options"
+            onClick={handleClickOptionsTab}>
+            {chrome.i18n.getMessage('options_name')}
+          </a>
+        </li>
+        <li className="nav-item">
+          <a
+            className={cx('nav-link', { active: props.activeTabId === 'about' })}
+            href="#about"
+            onClick={handleClickAboutTab}>
+            {chrome.i18n.getMessage('about_name')}
+          </a>
+        </li>
+      </ul>
+    </>
+  );
 }

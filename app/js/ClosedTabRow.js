519d7374376e74a38f91679188c2226479c6b338
Ross Allen <reallen@gmail.com>
2019-07-09 23:05:37 -0400

    Cut out code, use functional comp. for ClosedTabRow

--- app/js/ClosedTabRow.js
+++ app/js/ClosedTabRow.js
@@ -22,125 +22,102 @@ type Props = {
   style: Object,
 };
 
-type State = {
-  hovered: boolean,
-};
+export default function ClosedTabRow(props: Props) {
+  const { isSelected, session, style, tab } = props;
 
-export default class ClosedTabRow extends React.PureComponent<Props, State> {
-  constructor(props: Props) {
-    super(props);
-    this.state = {
-      hovered: false,
-    };
+  function _handleClickAnchor(event: SyntheticMouseEvent<HTMLElement>) {
+    event.preventDefault();
+    props.onOpenTab(tab, props.session);
   }
 
-  _handleClickAnchor = (event: SyntheticMouseEvent<HTMLElement>) => {
-    const { tab } = this.props;
-    event.preventDefault();
-    this.props.onOpenTab(tab, this.props.session);
-  };
-
-  _handleClickCheckbox = (event: SyntheticMouseEvent<HTMLInputElement>) => {
+  function _handleClickCheckbox(event: SyntheticMouseEvent<HTMLInputElement>) {
     // Dynamic type check to ensure target is an input element.
     if (!(event.target instanceof HTMLInputElement)) return;
+    props.onToggleTab(props.tab, event.target.checked, event.shiftKey);
+  }
 
-    this.props.onToggleTab(this.props.tab, event.target.checked, event.shiftKey);
-  };
+  function _handleClickRemove() {
+    props.onRemoveTab(props.tab);
+  }
 
-  _handleClickRemove = () => {
-    this.props.onRemoveTab(this.props.tab);
-  };
-
-  _handleClickTd = (event: SyntheticMouseEvent<HTMLElement>) => {
+  function _handleClickTd(event: SyntheticMouseEvent<HTMLElement>) {
     if (event.currentTarget.nodeName === 'input') return;
-    this.props.onToggleTab(this.props.tab, !this.props.isSelected, event.shiftKey);
-  };
+    props.onToggleTab(props.tab, !props.isSelected, event.shiftKey);
+  }
 
-  _handleMouseOut = () => {
-    this.setState({ hovered: false });
-  };
-
-  _handleMouseOver = () => {
-    this.setState({ hovered: true });
-  };
-
-  render() {
-    const { isSelected, session, style, tab } = this.props;
-
-    return (
+  return (
+    <div
+      aria-label="row"
+      className={cx('ReactVirtualized__Table__row', { 'table-warning': isSelected })}
+      role="row"
+      style={style}>
       <div
-        aria-label="row"
-        className={cx('ReactVirtualized__Table__row', { 'table-warning': isSelected })}
-        role="row"
-        style={style}>
-        <div
-          className="ReactVirtualized__Table__rowColumn"
-          onClick={this._handleClickTd}
-          style={{ verticalAlign: 'middle' }}>
-          <input
-            checked={isSelected}
-            className="checkbox--td"
-            onClick={this._handleClickCheckbox}
-            type="checkbox"
-          />
-        </div>
-        <div
-          className="faviconCol ReactVirtualized__Table__rowColumn"
-          style={{ verticalAlign: 'middle' }}>
-          <LazyImage
-            alt=""
-            className="faviconCol--hover-hidden favicon"
-            height={16}
-            src={tab.favIconUrl}
-            width={16}
-          />
-          <span
-            className="faviconCol--hover-shown"
-            onClick={this._handleClickRemove}
-            role="button"
-            style={{ cursor: 'pointer', height: 16, width: 16 }}
-            tabIndex={0}
-            title="Remove this tab">
-            <i className="fas fa-trash-alt" />
-          </span>
-        </div>
-        <div className="ReactVirtualized__Table__rowColumn py-1" style={{ flex: 1 }}>
-          <div style={{ display: 'flex' }}>
-            <div className="CorralTabRow-content">
-              <a
-                href={tab.url}
-                onClick={this._handleClickAnchor}
-                rel="noopener noreferrer"
-                style={{ flex: 1 }}
-                target="_blank"
-                title={tab.url}>
-                {tab.title}
-              </a>
-              <br />
-              <small className={cx({ 'text-muted': !isSelected })}>
-                ({tab.url == null ? '???' : extractHostname(tab.url)})
-              </small>
-            </div>
+        className="ReactVirtualized__Table__rowColumn"
+        onClick={_handleClickTd}
+        style={{ verticalAlign: 'middle' }}>
+        <input
+          checked={isSelected}
+          className="checkbox--td"
+          onClick={_handleClickCheckbox}
+          type="checkbox"
+        />
+      </div>
+      <div
+        className="faviconCol ReactVirtualized__Table__rowColumn"
+        style={{ verticalAlign: 'middle' }}>
+        <LazyImage
+          alt=""
+          className="faviconCol--hover-hidden favicon"
+          height={16}
+          src={tab.favIconUrl}
+          width={16}
+        />
+        <span
+          className="faviconCol--hover-shown"
+          onClick={_handleClickRemove}
+          role="button"
+          style={{ cursor: 'pointer', height: 16, width: 16 }}
+          tabIndex={0}
+          title="Remove this tab">
+          <i className="fas fa-trash-alt" />
+        </span>
+      </div>
+      <div className="ReactVirtualized__Table__rowColumn py-1" style={{ flex: 1 }}>
+        <div style={{ display: 'flex' }}>
+          <div className="CorralTabRow-content">
+            <a
+              href={tab.url}
+              onClick={_handleClickAnchor}
+              rel="noopener noreferrer"
+              style={{ flex: 1 }}
+              target="_blank"
+              title={tab.url}>
+              {tab.title}
+            </a>
+            <br />
+            <small className={cx({ 'text-muted': !isSelected })}>
+              ({tab.url == null ? '???' : extractHostname(tab.url)})
+            </small>
           </div>
         </div>
-        <div
-          className="ReactVirtualized__Table__rowColumn text-right"
-          style={{ verticalAlign: 'middle' }}
-          title={
-            /* $FlowFixMe: `closedAt` is an expando property added by Tab Wrangler to chrome$Tab */
-            new Date(tab.closedAt).toLocaleString()
-          }>
-          {/* $FlowFixMe: `closedAt` is an expando property added by Tab Wrangler to chrome$Tab */}
-          <TimeAgo datetime={tab.closedAt} locale={uiLanguage} />
-        </div>
-        <div className="ReactVirtualized__Table__rowColumn" style={{ width: '11px' }}>
-          {session == null ? null : (
-            <abbr title={chrome.i18n.getMessage('corral_tabSessionFresh')}>
-              <i className="fas fa-leaf text-success" />
-            </abbr>
-          )}
-        </div>
       </div>
-    );
-  }
+      <div
+        className="ReactVirtualized__Table__rowColumn text-right"
+        style={{ verticalAlign: 'middle' }}
+        title={
+          /* $FlowFixMe: `closedAt` is an expando property added by Tab Wrangler to chrome$Tab */
+          new Date(tab.closedAt).toLocaleString()
+        }>
+        {/* $FlowFixMe: `closedAt` is an expando property added by Tab Wrangler to chrome$Tab */}
+        <TimeAgo datetime={tab.closedAt} locale={uiLanguage} />
+      </div>
+      <div className="ReactVirtualized__Table__rowColumn" style={{ width: '11px' }}>
+        {session == null ? null : (
+          <abbr title={chrome.i18n.getMessage('corral_tabSessionFresh')}>
+            <i className="fas fa-leaf text-success" />
+          </abbr>
+        )}
+      </div>
+    </div>
+  );
 }

20911b0d23245775513202c80314e4bd0518c6ec
Ross Allen <reallen@gmail.com>
2019-07-11 23:08:56 -0400

    Sort first by window, then by tab index
    Tab order from `query` is not guaranteed. Sort first by `windowId` and
    then by `index` (which is within a window) in order to get a better
    "tab order".

--- app/js/LockTab.js
+++ app/js/LockTab.js
@@ -44,16 +44,28 @@ const ReverseChronoSorter: Sorter = {
 const TabOrderSorter: Sorter = {
   label: chrome.i18n.getMessage('tabLock_sort_tabOrder') || '',
   shortLabel: chrome.i18n.getMessage('tabLock_sort_tabOrder_short') || '',
-  sort() {
-    return 1;
+  sort(tabA, tabB) {
+    if (tabA == null || tabB == null) {
+      if (tabA != null && tabB == null) {
+        return 1;
+      } else if (tabA == null && tabB != null) {
+        return -1;
+      } else {
+        return 0;
+      }
+    } else if (tabA.windowId === tabB.windowId) {
+      return tabA.index - tabB.index;
+    } else {
+      return tabA.windowId - tabB.windowId;
+    }
   },
 };
 
 const ReverseTabOrderSorter: Sorter = {
   label: chrome.i18n.getMessage('tabLock_sort_tabOrder_desc') || '',
   shortLabel: chrome.i18n.getMessage('tabLock_sort_tabOrder_desc_short') || '',
-  sort() {
-    return -1;
+  sort(tabA, tabB) {
+    return -1 * TabOrderSorter.sort(tabA, tabB);
   },
 };
 

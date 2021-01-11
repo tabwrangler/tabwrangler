8a3bbcdcd01d8ee6facdf87a4ecb1ad78ab545ad
matthew.ed.keller@gmail.com <matthew.ed.keller@gmail.com>
2021-01-11 12:35:44 -0500

    Add a 'Grouped' tab lock reason

--- app/js/OpenTabRow.js
+++ app/js/OpenTabRow.js
@@ -40,6 +40,8 @@ export default function OpenTabRow(props: Props): React.Node {
       reason = chrome.i18n.getMessage("tabLock_lockedReason_pinned");
     } else if (settings.get("filterAudio") && tab.audible) {
       reason = <abbr title={chrome.i18n.getMessage("tabLock_lockedReason_audible")}>Locked</abbr>;
+    } else if (settings.get("filterGroupedTabs") && "groupId" in tab && tab.groupId > 0) {
+      reason = chrome.i18n.getMessage("tabLock_lockedReason_group");
     } else if (tabWhitelistMatch) {
       reason = (
         <abbr title={chrome.i18n.getMessage("tabLock_lockedReason_matches", tabWhitelistMatch)}>

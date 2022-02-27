be47aa1eb28b2d3a3de9f1747a931353455ef065
Tamas Kadar <ktamas@ktamas.com>
2022-02-27 15:57:52 +0200

    migrate to latest jest

---
+++ jest.config.js
@@ -0,0 +1,15 @@
+// Sync object
+/** @type {import('@jest/types').Config.InitialOptions} */
+
+module.exports = async () => {
+  return {
+    verbose: true,
+    testEnvironment: "jsdom",
+    globals: {
+      chrome: true,
+    },
+    moduleNameMapper: {
+      "\\.(s?css)$": "<rootDir>/app/__mocks__/styleMock.js",
+    },
+  };
+};

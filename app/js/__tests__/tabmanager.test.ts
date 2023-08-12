24eb216e4de44baef203e35dae85e49c49c31634
Ross Allen <rossgit@gmail.com>
2023-08-12 10:26:14 -0700

    Match tests to single `tabs.remove` call

--- app/js/__tests__/tabmanager.test.ts
+++ app/js/__tests__/tabmanager.test.ts
@@ -25,15 +25,16 @@ function createTab(overrides: Partial<chrome.tabs.Tab>): chrome.tabs.Tab {
 beforeEach(() => {
   window.chrome = {
     storage: {
-      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-      // @ts-ignore:next-line
+      // @ts-expect-error Only partial implementation of `chrome.storage.local` API
       local: {},
     },
-    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-    // @ts-ignore:next-line
-    tabs: {},
-    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-    // @ts-ignore:next-line
+    tabs: {
+      // @ts-expect-error Only partial implementation of `chrome.tabs.remove` API
+      remove: jest.fn((_tabIds, callback) => {
+        if (callback != null) callback();
+      }),
+    },
+    // @ts-expect-error Only partial implementation of `chrome.browserAction` API
     browserAction: {},
   };
 });
@@ -51,37 +52,28 @@ describe("wrangleTabs", () => {
     store = configureMockStore();
   });
 
-  test("wrangles new tabs", () => {
+  test("wrangles new tabs", async () => {
     // eslint-disable-next-line @typescript-eslint/ban-ts-comment
     // @ts-ignore:next-line
     settings.get = jest.fn(() => 5); //maxTabs
-    window.chrome.tabs.remove = jest.fn();
     const tabManager = new TabManager(<any>store);
 
     const testTabs = [createTab({ id: 2 }), createTab({ id: 3 }), createTab({ id: 4 })];
-    tabManager.wrangleTabs(testTabs);
+    await tabManager.wrangleTabs(testTabs);
 
-    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-    // @ts-ignore:next-line
-    expect(window.chrome.tabs.remove.mock.calls.length).toBe(3);
-    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-    // @ts-ignore:next-line
-    expect(window.chrome.tabs.remove.mock.calls).toEqual([[2], [3], [4]]);
+    expect(window.chrome.tabs.remove).toHaveBeenCalledTimes(1);
+    expect(window.chrome.tabs.remove).toHaveBeenCalledWith([2, 3, 4], expect.anything());
 
     const setSavedTabsAction = store
-      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-      // @ts-ignore:next-line
       .getActions()
       .find((action: Action) => action.type === "SET_SAVED_TABS");
     expect(setSavedTabsAction).toBeDefined();
     expect(setSavedTabsAction.savedTabs.map((tab: chrome.tabs.Tab) => tab.id)).toEqual([4, 3, 2]);
   });
 
-  test("wrangles max tabs", () => {
-    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-    // @ts-ignore:next-line
+  test("wrangles max tabs", async () => {
+    // @ts-expect-error Only partial implementation of `settings` API
     settings.get = jest.fn(() => 3);
-    window.chrome.tabs.remove = jest.fn();
     const tabManager = new TabManager(<any>store);
 
     const testTabs = [
@@ -93,28 +85,21 @@ describe("wrangleTabs", () => {
     window.chrome.storage.local.set = jest.fn();
 
     window.chrome.browserAction.setBadgeText = jest.fn();
-    tabManager.wrangleTabs(testTabs);
+    await tabManager.wrangleTabs(testTabs);
 
-    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-    // @ts-ignore:next-line
-    expect(window.chrome.tabs.remove.mock.calls.length).toBe(4);
-    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-    // @ts-ignore:next-line
-    expect(window.chrome.tabs.remove.mock.calls).toEqual([[2], [3], [4], [5]]);
-    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-    // @ts-ignore:next-line
+    expect(window.chrome.tabs.remove).toHaveBeenCalledTimes(1);
+    expect(window.chrome.tabs.remove).toHaveBeenCalledWith([2, 3, 4, 5], expect.anything());
     expect(store.getActions()).toContainEqual({
       totalTabsWrangled: 4,
       type: "SET_TOTAL_TABS_WRANGLED",
     });
   });
 
-  test("replaces duplicate tab in the corral if exact URL matches", () => {
+  test("replaces duplicate tab in the corral if exact URL matches", async () => {
     settings.get = jest
       .fn()
       .mockImplementationOnce(() => 100)
       .mockImplementationOnce(() => "exactURLMatch");
-    window.chrome.tabs.remove = jest.fn();
     store = configureMockStore({
       localStorage: {
         savedTabs: [
@@ -129,35 +114,23 @@ describe("wrangleTabs", () => {
     window.chrome.storage.local.set = jest.fn();
     window.chrome.browserAction.setBadgeText = jest.fn();
 
-    // reset all mocks
-    jest.clearAllMocks();
-
     const testTabs = [createTab({ id: 4, url: "https://www.nytimes.com" })];
 
-    tabManager.wrangleTabs(testTabs);
+    await tabManager.wrangleTabs(testTabs);
 
-    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-    // @ts-ignore:next-line
-    expect(window.chrome.tabs.remove.mock.calls.length).toBe(1);
-    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-    // @ts-ignore:next-line
-    expect(window.chrome.tabs.remove.mock.calls).toEqual([[4]]);
-    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-    // @ts-ignore:next-line
+    expect(window.chrome.tabs.remove).toHaveBeenCalledTimes(1);
+    expect(window.chrome.tabs.remove).toHaveBeenCalledWith([4], expect.anything());
     expect(store.getActions()).toContainEqual({
       totalTabsWrangled: 1,
       type: "SET_TOTAL_TABS_WRANGLED",
     });
-
-    // TODO: Test for tabs
   });
 
-  test("replaces duplicate tab in the corral if hostname and title match", () => {
+  test("replaces duplicate tab in the corral if hostname and title match", async () => {
     settings.get = jest
       .fn()
       .mockImplementationOnce(() => 100)
       .mockImplementationOnce(() => "hostnameAndTitleMatch");
-    window.chrome.tabs.remove = jest.fn();
     store = configureMockStore({
       localStorage: {
         savedTabs: [
@@ -172,29 +145,20 @@ describe("wrangleTabs", () => {
     window.chrome.storage.local.set = jest.fn();
     window.chrome.browserAction.setBadgeText = jest.fn();
 
-    // reset all mocks
     jest.clearAllMocks();
 
     const testTabs = [
       createTab({ id: 4, url: "https://www.nytimes.com", title: "New York Times" }),
     ];
 
-    tabManager.wrangleTabs(testTabs);
+    await tabManager.wrangleTabs(testTabs);
 
-    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-    // @ts-ignore:next-line
-    expect(window.chrome.tabs.remove.mock.calls.length).toBe(1);
-    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-    // @ts-ignore:next-line
-    expect(window.chrome.tabs.remove.mock.calls).toEqual([[4]]);
-    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-    // @ts-ignore:next-line
+    expect(window.chrome.tabs.remove).toHaveBeenCalledTimes(1);
+    expect(window.chrome.tabs.remove).toHaveBeenCalledWith([4], expect.anything());
     expect(store.getActions()).toContainEqual({
       totalTabsWrangled: 1,
       type: "SET_TOTAL_TABS_WRANGLED",
     });
-
-    // TODO: Test for tabs
   });
 });
 

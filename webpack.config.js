3cc54b94d6458b5e5ba5e86ad88c3001d51770f9
Ross Allen <rossgit@gmail.com>
2023-09-21 21:55:35 -0400

    Use devtool recommended by Webpack
    `eval-source-map` is the devtool recommended by
    [Webpack's devtool docs][0].
    
    [0]: https://webpack.js.org/configuration/devtool/

--- webpack.config.js
+++ webpack.config.js
@@ -9,7 +9,7 @@ const package = require("./package.json");
 const webpack = require("webpack");
 
 const COMMON_CONFIG = {
-  devtool: "source-map",
+  devtool: "eval-source-map",
   entry: {
     background: "./app/background.ts",
     popup: "./app/popup.tsx",

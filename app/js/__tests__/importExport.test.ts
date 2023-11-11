c72ca56662fe09ea86339bfb7b0c4c8acbf7afb4
Ross Allen <rossgit@gmail.com>
2023-11-11 08:44:48 -0600

    Use latest actions in tests; no more `store`

--- app/js/__tests__/importExport.test.ts
+++ app/js/__tests__/importExport.test.ts
@@ -1,192 +1,129 @@
-// import { exportData, exportFileName, importData } from "../actions/importExportActions";
+import { exportData, exportFileName, importData } from "../actions/importExportActions";
 
-// describe("importExportActions", () => {
-//   test("should export the bookmark data", () => {
-//     store = configureMockStore({
-//       tempStorage: {
-//         totalTabsRemoved: 256,
-//         totalTabsUnwrangled: 120,
-//         totalTabsWrangled: 100,
-//       },
-//     });
+describe("importExportActions", () => {
+  beforeEach(() => {
+    jest.clearAllMocks();
+  });
 
-//     const ogGet = chrome.storage.local.get;
+  test("exports the bookmark data", async () => {
+    await chrome.storage.local.set({
+      "persist:localStorage": {
+        totalTabsRemoved: 256,
+        totalTabsUnwrangled: 120,
+        totalTabsWrangled: 100,
+      },
+    });
 
-//     // @ts-expect-error Incomplete `get` implementation
-//     chrome.storage.local.get = (t, func) => {
-//       func({ test: 2 });
-//     };
+    const blob = await exportData();
+    expect(blob.type).toBe("application/json;charset=utf-8");
+  });
 
-//     // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-//     // @ts-ignore:next-line
-//     store.dispatch(exportData()).then((blob: Blob) => {
-//       expect(blob.type).toBe("application/json;charset=utf-8");
-//     });
+  test("imports the bookmark data", async () => {
+    const expectedImportData = {
+      savedTabs: [
+        {
+          active: false,
+          audible: false,
+          autoDiscardable: true,
+          closedAt: 1493418190099,
+          discarded: false,
+          height: 175,
+          highlighted: false,
+          id: 36,
+          incognito: false,
+          index: 1,
+          mutedInfo: {
+            muted: false,
+          },
+          pinned: false,
+          selected: false,
+          status: "complete",
+          title: "fish: Tutorial",
+          url: "https://fishshell.com/docs/current/tutorial.html",
+          width: 400,
+          windowId: 33,
+        },
+      ],
+      totalTabsRemoved: 256,
+      totalTabsUnwrangled: 16,
+      totalTabsWrangled: 32,
+    };
 
-//     chrome.storage.local.get = ogGet;
-//   });
+    const blob = new Blob([JSON.stringify(expectedImportData)], {
+      type: "text/plain;charset=utf-8",
+    });
 
-//   test("should import the bookmark data", (done) => {
-//     const expectedImportData = {
-//       savedTabs: [
-//         {
-//           active: false,
-//           audible: false,
-//           autoDiscardable: true,
-//           closedAt: 1493418190099,
-//           discarded: false,
-//           height: 175,
-//           highlighted: false,
-//           id: 36,
-//           incognito: false,
-//           index: 1,
-//           mutedInfo: {
-//             muted: false,
-//           },
-//           pinned: false,
-//           selected: false,
-//           status: "complete",
-//           title: "fish: Tutorial",
-//           url: "https://fishshell.com/docs/current/tutorial.html",
-//           width: 400,
-//           windowId: 33,
-//         },
-//       ],
-//       totalTabsRemoved: 256,
-//       totalTabsUnwrangled: 16,
-//       totalTabsWrangled: 32,
-//     };
+    await importData({
+      target: {
+        // @ts-expect-error Using only necessary key from `EventTarget`, no need to create real one
+        files: [blob],
+      },
+    });
 
-//     const blob = new Blob([JSON.stringify(expectedImportData)], {
-//       type: "text/plain;charset=utf-8",
-//     });
+    const { "persist:localStorage": data } = await chrome.storage.local.get("persist:localStorage");
+    expect(data).toEqual(expectedImportData);
+  });
 
-//     store
-//       .dispatch(
-//         // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-//         // @ts-ignore:next-line
-//         importData({
-//           target: {
-//             // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-//             // @ts-ignore:next-line
-//             files: [blob],
-//           },
-//         })
-//       )
-//       .then(() => {
-//         expect(store.getActions()).toEqual([
-//           { totalTabsRemoved: 256, type: "SET_TOTAL_TABS_REMOVED" },
-//           { totalTabsUnwrangled: 16, type: "SET_TOTAL_TABS_UNWRANGLED" },
-//           { totalTabsWrangled: 32, type: "SET_TOTAL_TABS_WRANGLED" },
-//           {
-//             savedTabs: [
-//               {
-//                 active: false,
-//                 audible: false,
-//                 autoDiscardable: true,
-//                 closedAt: 1493418190099,
-//                 discarded: false,
-//                 height: 175,
-//                 highlighted: false,
-//                 id: 36,
-//                 incognito: false,
-//                 index: 1,
-//                 mutedInfo: { muted: false },
-//                 pinned: false,
-//                 selected: false,
-//                 status: "complete",
-//                 title: "fish: Tutorial",
-//                 url: "https://fishshell.com/docs/current/tutorial.html",
-//                 width: 400,
-//                 windowId: 33,
-//               },
-//             ],
-//             type: "SET_SAVED_TABS",
-//           },
-//         ]);
-//         done();
-//       })
-//       .catch((e: Error) => {
-//         console.error(e);
-//         done();
-//       });
-//   });
+  test("fails to import non existent backup", async () => {
+    try {
+      await importData({
+        target: {
+          // @ts-expect-error Using only necessary key from `EventTarget`, no need to create real one
+          files: [],
+        },
+      });
+      fail("Expected `importData` failure: no files");
+    } catch (error) {
+      expect(chrome.storage.local.set).not.toHaveBeenCalled();
+    }
+  });
 
-//   test("should fail to import non existent backup", (done) => {
-//     store
-//       .dispatch(
-//         // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-//         // @ts-ignore:next-line
-//         importData({
-//           target: {
-//             // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-//             // @ts-ignore:next-line
-//             files: [],
-//           },
-//         })
-//       )
-//       .catch(() => {
-//         expect(chrome.storage.local.set).not.toHaveBeenCalled();
-//         done();
-//       });
-//   });
+  test("should fail import of incomplete backup data", async () => {
+    // this is missing the savedTabs object
+    const expectedImportData = [
+      { totalTabsRemoved: 256 },
+      { totalTabsUnwrangled: 16 },
+      { totalTabsWrangled: 32 },
+    ];
 
-//   test("should fail import of incomplete backup data", (done) => {
-//     // this is missing the savedTabs object
-//     const expectedImportData = [
-//       { totalTabsRemoved: 256 },
-//       { totalTabsUnwrangled: 16 },
-//       { totalTabsWrangled: 32 },
-//     ];
+    const blob = new Blob([JSON.stringify(expectedImportData)], {
+      type: "text/plain;charset=utf-8",
+    });
 
-//     const blob = new Blob([JSON.stringify(expectedImportData)], {
-//       type: "text/plain;charset=utf-8",
-//     });
+    try {
+      await importData({
+        target: {
+          // @ts-expect-error Using only necessary key from `EventTarget`, no need to create real one
+          files: [blob],
+        },
+      });
+      fail("Expected `importData` failure: missing `savedTabs` key");
+    } catch (error) {
+      expect(chrome.storage.local.set).not.toHaveBeenCalled();
+    }
+  });
 
-//     store
-//       .dispatch(
-//         // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-//         // @ts-ignore:next-line
-//         importData({
-//           target: {
-//             // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-//             // @ts-ignore:next-line
-//             files: [blob],
-//           },
-//         })
-//       )
-//       .catch(() => {
-//         expect(chrome.storage.local.set).not.toHaveBeenCalled();
-//         done();
-//       });
-//   });
+  test("fails import of corrupt backup data", async () => {
+    const blob = new Blob(["{345:}"], {
+      type: "text/plain;charset=utf-8",
+    });
 
-//   test("should fail import of corrupt backup data", (done) => {
-//     const blob = new Blob(["{345:}"], {
-//       type: "text/plain;charset=utf-8",
-//     });
+    try {
+      await importData({
+        target: {
+          // @ts-expect-error Using only necessary key from `EventTarget`, no need to create real one
+          files: [blob],
+        },
+      });
+      fail("Expected `importData` failure: corrupted `Blob` backup data");
+    } catch (error) {
+      expect(chrome.storage.local.set).not.toHaveBeenCalled();
+    }
+  });
 
-//     store
-//       .dispatch(
-//         // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-//         // @ts-ignore:next-line
-//         importData({
-//           target: {
-//             // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-//             // @ts-ignore:next-line
-//             files: [blob],
-//           },
-//         })
-//       )
-//       .catch(() => {
-//         expect(chrome.storage.local.set).not.toHaveBeenCalled();
-//         done();
-//       });
-//   });
-
-//   test("should generate a unique file name based on a given date", () => {
-//     const date = new Date("2017-04-10 00:00:00 GMT");
-//     const uniqueFileName = exportFileName(date);
-//     expect(uniqueFileName).toBe("TabWranglerExport-2017-04-10.json");
-//   });
-// });
+  test("generates a unique file name based on a given date", () => {
+    const date = new Date("2017-04-10 00:00:00 GMT");
+    const uniqueFileName = exportFileName(date);
+    expect(uniqueFileName).toBe("TabWranglerExport-2017-04-10.json");
+  });
+});

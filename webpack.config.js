c883940848dfaad6b945d05c925ce36df4a56db8
Ross Allen <ssorallen@users.noreply.github.com>
2024-04-21 00:39:09 -0500

    Upgrade to Webpack v5, eliminate SSL issues (#460)
    This enables dropping the `--openssl-legacy-provider` Node flag because
    the updated libraries use newer SSL providers. This also enables newer
    Webpack features because v5 has been out for quite a long time.

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,9 @@
 /* eslint-env node */
 
 const CopyWebpackPlugin = require("copy-webpack-plugin");
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const path = require("path");
 const package = require("./package.json");
 const webpack = require("webpack");
@@ -25,35 +27,32 @@ const COMMON_CONFIG = {
       },
       {
         test: /\.s?css$/,
-        use: [
-          "style-loader",
-          "css-loader",
-          {
-            loader: "postcss-loader",
-            options: {
-              postcssOptions: {
-                plugins() {
-                  return [require("autoprefixer")];
-                },
-              },
-            },
-          },
-          "sass-loader",
-        ],
+        use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
       },
       {
-        test: /\.(eot|svg|ttf|woff|woff2)$/,
+        test: /\.woff2?$/i,
+        type: "asset/resource",
+        dependency: { not: ["url"] },
+      },
+      {
+        test: /\.(svg)$/,
         use: ["file-loader"],
       },
     ],
   },
+  optimization: {
+    minimizer: [new CssMinimizerPlugin()],
+  },
   plugins: [
-    new CopyWebpackPlugin([
-      { from: "_locales/**" },
-      { from: "app/img/", to: "img/" },
-      { from: "MIT-LICENSE.txt" },
-      { from: "README.md" },
-    ]),
+    new CopyWebpackPlugin({
+      patterns: [
+        { from: "_locales/**" },
+        { from: "app/img/", to: "img/" },
+        { from: "MIT-LICENSE.txt" },
+        { from: "README.md" },
+      ],
+    }),
+    new MiniCssExtractPlugin(),
     new HtmlWebpackPlugin({
       cache: false, // Disable cache to always create file in multi-compiler build
       chunks: ["popup"],
@@ -75,27 +74,29 @@ module.exports = [
       filename: "[name].entry.js",
     },
     plugins: COMMON_CONFIG.plugins.concat([
-      new CopyWebpackPlugin([
-        {
-          from: "app/manifest.template.json",
-          to: "manifest.json",
-          transform(buffer) {
-            const manifest = JSON.parse(buffer.toString());
+      new CopyWebpackPlugin({
+        patterns: [
+          {
+            from: "app/manifest.template.json",
+            to: "manifest.json",
+            transform(buffer) {
+              const manifest = JSON.parse(buffer.toString());
 
-            // Use background script as a ServiceWorker, as required by Chrome's Manifest v3
-            // implementation.
-            // See https://developer.chrome.com/docs/extensions/migrating/to-service-workers/
-            manifest.background = {
-              service_worker: "background.entry.js",
-            };
+              // Use background script as a ServiceWorker, as required by Chrome's Manifest v3
+              // implementation.
+              // See https://developer.chrome.com/docs/extensions/migrating/to-service-workers/
+              manifest.background = {
+                service_worker: "background.entry.js",
+              };
 
-            // Sync extension version to the version in package.json.
-            manifest.version = package.version;
+              // Sync extension version to the version in package.json.
+              manifest.version = package.version;
 
-            return JSON.stringify(manifest, null, 2);
+              return JSON.stringify(manifest, null, 2);
+            },
           },
-        },
-      ]),
+        ],
+      }),
       new webpack.DefinePlugin({
         EXTENSION_URL: JSON.stringify(
           "https://chrome.google.com/webstore/detail/egnjhciaieeiiohknchakcodbpgjnchh/",
@@ -111,36 +112,38 @@ module.exports = [
       filename: "[name].entry.js",
     },
     plugins: COMMON_CONFIG.plugins.concat([
-      new CopyWebpackPlugin([
-        {
-          from: "app/manifest.template.json",
-          to: "manifest.json",
-          transform(buffer) {
-            const manifest = JSON.parse(buffer.toString());
+      new CopyWebpackPlugin({
+        patterns: [
+          {
+            from: "app/manifest.template.json",
+            to: "manifest.json",
+            transform(buffer) {
+              const manifest = JSON.parse(buffer.toString());
 
-            // Use background script as intended because Firefox supports background scripts in
-            // its Manifest v3 implementation.
-            // See https://developer.mozilla.org/en-US/docs/Mozilla/Add-ons/WebExtensions/Background_scripts
-            manifest.background = {
-              scripts: ["background.entry.js"],
-            };
+              // Use background script as intended because Firefox supports background scripts in
+              // its Manifest v3 implementation.
+              // See https://developer.mozilla.org/en-US/docs/Mozilla/Add-ons/WebExtensions/Background_scripts
+              manifest.background = {
+                scripts: ["background.entry.js"],
+              };
 
-            // Firefox (Gecko) settings
-            // See https://developer.mozilla.org/en-US/docs/Mozilla/Add-ons/WebExtensions/manifest.json/browser_specific_settings
-            manifest.browser_specific_settings = {
-              gecko: {
-                id: "{81b74d53-9416-4fb3-afa2-ab46684b253b}",
-                strict_min_version: "109.0",
-              },
-            };
+              // Firefox (Gecko) settings
+              // See https://developer.mozilla.org/en-US/docs/Mozilla/Add-ons/WebExtensions/manifest.json/browser_specific_settings
+              manifest.browser_specific_settings = {
+                gecko: {
+                  id: "{81b74d53-9416-4fb3-afa2-ab46684b253b}",
+                  strict_min_version: "109.0",
+                },
+              };
 
-            // Sync extension version to the version in package.json.
-            manifest.version = package.version;
+              // Sync extension version to the version in package.json.
+              manifest.version = package.version;
 
-            return JSON.stringify(manifest, null, 2);
+              return JSON.stringify(manifest, null, 2);
+            },
           },
-        },
-      ]),
+        ],
+      }),
       new webpack.DefinePlugin({
         EXTENSION_URL: JSON.stringify(
           "https://addons.mozilla.org/en-US/firefox/addon/tabwrangler/",
--- webpack.production.config.js
+++ webpack.production.config.js
@@ -1,25 +1,18 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 /* eslint-env node */
 
-const ArchivePlugin = require("webpack-archive-plugin");
-const OptimizeCssAssetsPlugin = require("optimize-css-assets-webpack-plugin");
+const ZipWebpackPlugin = require("zip-webpack-plugin");
 const developmentConfig = require("./webpack.config.js");
 const packageJson = require("./package.json");
-const path = require("path");
 
 module.exports = developmentConfig.map(function (platformConfig) {
   return Object.assign({}, platformConfig, {
     devtool: false,
     mode: "production",
     plugins: platformConfig.plugins.concat([
-      new OptimizeCssAssetsPlugin(),
-      new ArchivePlugin({
-        format: "zip",
-        output: path.join(
-          __dirname,
-          "dist",
-          `tabwrangler-${platformConfig.name}-${packageJson.version}`,
-        ),
+      new ZipWebpackPlugin({
+        filename: `tabwrangler-${platformConfig.name}-${packageJson.version}.zip`,
+        path: "..",
       }),
     ]),
   });

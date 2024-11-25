5ed54be3f22833a68c881c8a095c92ef862a3c54
Fergus <95570212+F-Joubert@users.noreply.github.com>
2024-11-25 18:49:50 +1100

    Removed check for url only and set default to matching tabs by both url and title. Added tab title parameter to getWhiteListMatch method

--- app/js/tabUtil.ts
+++ app/js/tabUtil.ts
@@ -31,15 +31,10 @@ export function getURLPositionFilterByWrangleOption(
   savedTabs: chrome.tabs.Tab[],
   option: WrangleOption,
 ): (tab: chrome.tabs.Tab) => number {
-  if (option === "hostnameAndTitleMatch") {
     return (tab: chrome.tabs.Tab): number =>
       findPositionByHostnameAndTitle(savedTabs, tab.url, tab.title);
-  } else if (option === "exactURLMatch") {
-    return (tab: chrome.tabs.Tab): number => findPositionByURL(savedTabs, tab.url);
-  }
-
   // `'withDupes'` && default
-  return () => -1;
+  // return () => -1;
 }
 
 // Note: Mutates `storageLocalPersist`!
@@ -150,13 +145,26 @@ export async function updateLastAccessed(tabOrTabId: chrome.tabs.Tab | number): 
   }
 }
 
+// export function getWhitelistMatch(
+//   url: string | undefined,
+//   { whitelist }: { whitelist: string[] },
+// ): string | null {
+//   if (url == null) return null;
+//   for (let i = 0; i < whitelist.length; i++) {
+//     if (url.indexOf(whitelist[i]) !== -1) {
+//       return whitelist[i];
+//     }
+//   }
+//   return null;
+// }
 export function getWhitelistMatch(
   url: string | undefined,
+  title: string | undefined,
   { whitelist }: { whitelist: string[] },
 ): string | null {
   if (url == null) return null;
   for (let i = 0; i < whitelist.length; i++) {
-    if (url.indexOf(whitelist[i]) !== -1) {
+    if (url.indexOf(whitelist[i]) !== -1 && title == settings.targetTitle) {
       return whitelist[i];
     }
   }
@@ -172,10 +180,10 @@ export function isTabLocked(
     whitelist,
   }: { filterAudio: boolean; filterGroupedTabs: boolean; lockedIds: number[]; whitelist: string[] },
 ): boolean {
-  const tabWhitelistMatch = getWhitelistMatch(tab.url, { whitelist });
+  const tabWhitelistMatch = getWhitelistMatch(tab.url, tab.title, { whitelist });
   return (
     tab.pinned ||
-    !!tabWhitelistMatch ||
+    !tabWhitelistMatch ||
     (tab.id != null && lockedIds.indexOf(tab.id) !== -1) ||
     !!(filterGroupedTabs && "groupId" in tab && tab.groupId > 0) ||
     !!(tab.audible && filterAudio)

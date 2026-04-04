267535129c7e440ccfbf243bb2fcbcb52145c893
Ross Allen <rossgit@gmail.com>
2026-04-04 10:58:53 -0400

    Import correct favicon file
    Fixes issue from https://github.com/tabwrangler/tabwrangler/commit/800bb103c4a7649de00dd9a6d3013fca12047612

--- app/js/TabFavicon.tsx
+++ app/js/TabFavicon.tsx
@@ -7,7 +7,8 @@ import type { FaviconApiFaviconProps } from "./FaviconApiFavicon";
 // * Chrome: supports a custom favicon API for loading from disk for pages in browser history
 // * Firefox: has no custom favicon support, favicons must be fetched over the internet
 const TabFavicon = HAS_FAVICON_API
-  ? (require("./ChromeFavicon") as { default: React.ComponentType<FaviconApiFaviconProps> }).default
+  ? (require("./FaviconApiFavicon") as { default: React.ComponentType<FaviconApiFaviconProps> })
+      .default
   : (require("./LazyImage/LazyImage") as { default: React.ComponentType<LazyImageProps> }).default;
 
 export const TabFaviconProvider: React.ComponentType<LazyImageProviderProps> = HAS_FAVICON_API
